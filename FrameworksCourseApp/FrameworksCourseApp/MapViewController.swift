a1cad4f721e89191a994c8244a2963605dab9fd7
Denis <recmanus@gmail.com>
2022-03-10 16:21:12 +0300

    Lesson1HomeWorkStage3
    Made a marker and configuration of map functions

--- FrameworksCourseApp/FrameworksCourseApp/MapViewController.swift
+++ FrameworksCourseApp/FrameworksCourseApp/MapViewController.swift
@@ -12,11 +12,34 @@ class MapViewController: UIViewController {
 
     @IBOutlet weak var mapView: GMSMapView!
     
+    var marker: GMSMarker?
+    let coordinates = CLLocationCoordinate2D(latitude: 55.7282982, longitude: 37.5779991)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        configureMap()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+       // addMarker()
+    }
+    
+    func addMarker() {
+        marker = GMSMarker(position: coordinates)
+        marker?.icon = GMSMarker.markerImage(with: .systemPink)
+        marker?.map = mapView
+    }
+    
+    func configureMap() {
+        let cameraPosition = GMSCameraPosition.camera(withTarget: coordinates, zoom: 17)
+        mapView.camera = cameraPosition
     }
 
-
+    @IBAction func createMarkerButton(_ sender: Any) {
+        addMarker()
+    }
+    
 }
 
