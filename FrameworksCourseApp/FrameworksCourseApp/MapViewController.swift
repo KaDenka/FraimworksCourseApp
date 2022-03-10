89338105d4b038cf8fb641c815b2d62c11260906
Denis <recmanus@gmail.com>
2022-03-10 13:09:34 +0300

    Lesson1HomeWorkStage1
    Install pod GoogleMaps

---
+++ FrameworksCourseApp/FrameworksCourseApp/MapViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MapViewController.swift
+//  FrameworksCourseApp
+//
+//  Created by Denis Kazarin on 10.03.2022.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FrameworksCourseApp/Pods/Target Support Files/Pods-FrameworksCourseApp-FrameworksCourseAppUITests/Pods-FrameworksCourseApp-FrameworksCourseAppUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FrameworksCourseApp_FrameworksCourseAppUITests : NSObject
+@end
+@implementation PodsDummy_Pods_FrameworksCourseApp_FrameworksCourseAppUITests
+@end
