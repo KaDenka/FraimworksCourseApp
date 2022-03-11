3a846ea2e7dea8fc2fcf431d8962d72956ef1584
Denis <recmanus@gmail.com>
2022-03-11 13:13:37 +0300

    Lesson1HomeWorkStage4
    Add functions for adding auto and manual markers

---
+++ FrameworksCourseApp/FrameworksCourseApp/Controllers/MapViewController.swift
@@ -0,0 +1,78 @@
+//
+//  MapViewController.swift
+//  FrameworksCourseApp
+//
+//  Created by Denis Kazarin on 10.03.2022.
+//
+
+import UIKit
+import GoogleMaps
+
+enum MarkerSelected {
+    case autoMarker
+    case manualMarker
+}
+
+class MapViewController: UIViewController {
+    
+    @IBOutlet weak var mapView: GMSMapView!
+    
+    var marker: GMSMarker?
+    var manualMarker: GMSMarker?
+    
+    var coordinates = CLLocationCoordinate2D(latitude: 55.7282982, longitude: 37.5779991)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureMap()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        mapView.delegate = self
+    }
+    
+    func addMarker(_ markerType: MarkerSelected, _ coordinate: CLLocationCoordinate2D) {
+        
+        switch markerType {
+        case .autoMarker:
+            marker = GMSMarker(position: coordinate)
+            marker?.icon = GMSMarker.markerImage(with: .systemPink)
+            marker?.title = "Auto Position"
+            marker?.snippet = "Actual selected auto marker"
+            marker?.map = mapView
+        case .manualMarker:
+            manualMarker = GMSMarker(position: coordinate)
+            manualMarker?.icon = GMSMarker.markerImage(with: .systemGreen)
+            manualMarker?.title = "Manual Position"
+            manualMarker?.snippet = "Actual selected manual marker"
+            manualMarker?.map = mapView
+        }
+    }
+    
+    func removeMarker() {
+        marker?.map = nil
+        marker = nil
+    }
+    
+    func configureMap() {
+        let cameraPosition = GMSCameraPosition.camera(withTarget: coordinates, zoom: 17)
+        mapView.camera = cameraPosition
+    }
+    
+    @IBAction func createMarkerButton(_ sender: Any) {
+        coordinates = mapView.camera.target
+        //    marker == nil ? addMarker(coordinates) : removeMarker()
+        addMarker(.autoMarker, coordinates)
+        
+    }
+    
+}
+
+extension MapViewController: GMSMapViewDelegate {
+    func mapView(_ mapView: GMSMapView, didTapAt coordinate: CLLocationCoordinate2D) {
+        addMarker(.manualMarker, coordinate)
+    }
+}
+
+
+
