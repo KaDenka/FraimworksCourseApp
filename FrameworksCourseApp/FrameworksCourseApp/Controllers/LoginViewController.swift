fe0b9da3088874c6c6ba35aad2c429b44b88884f
Denis <recmanus@gmail.com>
2022-03-28 14:21:45 +0300

    Lesson3_Stage2
    Editing loginScreen

--- FrameworksCourseApp/FrameworksCourseApp/Controllers/LoginViewController.swift
+++ FrameworksCourseApp/FrameworksCourseApp/Controllers/LoginViewController.swift
@@ -9,21 +9,19 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var passwordButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+    }
+    @IBAction func loginButtonTapped(_ sender: UIButton) {
+    }
+    @IBAction func registrationButtonTapped(_ sender: UIButton) {
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
